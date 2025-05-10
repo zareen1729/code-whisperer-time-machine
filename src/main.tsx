8cc2274d37557a51254575eb6d97ea0b3531f7fa
zareen1729 <zareen1729@gmail.com>
2025-05-09 21:16:55 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-
 ---
 
 ### 📄 `src/main.tsx`
@@ -9,4 +8,3 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
-
